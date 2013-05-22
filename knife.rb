edc6865bd4d10b33262f4300c21cb1934a90c4ea
Stanley Kamithi <skamithi@gmail.com>
2013-05-22 16:50:55 -0400

    change root path to be absolute not relative in knife.rb

--- knife.rb
+++ knife.rb
@@ -1,5 +1,7 @@
-root = File.join(File.dirname(__FILE__))
+root = File.dirname(File.absolute_path(__FILE__))
+
 # knife solo will install new cookbooks in the first cookbook path found
+
 cookbook_path "#{root}/site-cookbooks", "#{root}/cookbooks"
 data_bag_path root + '/data_bags'
 knife[:secret_file] = root + '/solo.pem'
