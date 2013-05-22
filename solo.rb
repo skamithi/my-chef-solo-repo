039d0a0bc4fe008109d68612e904012e90cfe676
Stanley Kamithi <skamithi@gmail.com>
2013-05-22 16:53:42 -0400

    this is the file used by chef-solo on the client. so changed root path to /tmp/chef. it will create this directory and avoid an error where create_path is undefined due to nilclass.

--- solo.rb
+++ solo.rb
@@ -1,8 +1,8 @@
-root = File.join(File.dirname(__FILE__))
+root =  '/tmp/chef'
 
 file_cache_path           root
 data_bag_path             root + "/data_bags"
 encrypted_data_bag_secret root + "/data_bag_key"
 cookbook_path             [ root + "/site-cookbooks",
-                            root + "site-cookbooks" ]
+                            root + "/cookbooks" ]
 role_path                 root + "/roles"
