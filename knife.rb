a98e556cc84c63644706bdf29fe6e352fe90cd04
Stanley Kamithi <skamithi@gmail.com>
2013-05-22 10:50:50 -0400

    when issuing knife cookbook create, install the new cookbook in the site-cookbooks folder

--- knife.rb
+++ knife.rb
@@ -1,5 +1,5 @@
 root = File.join(File.dirname(__FILE__))
-cookbook_path root + '/cookbooks'
+# knife solo will install new cookbooks in the first cookbook path found
+cookbook_path "#{root}/site-cookbooks", "#{root}/cookbooks"
 data_bag_path root + '/data_bags'
-
 knife[:secret_file] = root + '/solo.pem'
