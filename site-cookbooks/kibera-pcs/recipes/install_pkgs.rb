0d145bbd9e317a3bd8f868d71eba885ab8ba8e6c
Stanley Kamithi <skamithi@gmail.com>
2013-05-22 22:02:26 -0400

    add medibuntu pkgs to kibera pc cookbook

--- site-cookbooks/kibera-pcs/recipes/install_pkgs.rb
+++ site-cookbooks/kibera-pcs/recipes/install_pkgs.rb
@@ -6,5 +6,7 @@ execute "install medibuntu" do
   command "sudo -E wget --output-document=/etc/apt/sources.list.d/medibuntu.list http://www.medibuntu.org/sources.list.d/$(lsb_release -cs).list && sudo apt-get --quiet update && sudo apt-get --yes --quiet --allow-unauthenticated install medibuntu-keyring && sudo apt-get --quiet update"
 end
 
-
+%w{libdvdcss2 mplayer-gui non-free-codecs w32codecs}.each do |w|
+  package w
+end
 
--- site-cookbooks/kibera-pcs/spec/install_pkgs_spec.rb
+++ site-cookbooks/kibera-pcs/spec/install_pkgs_spec.rb
@@ -2,7 +2,8 @@ require 'chefspec'
 
 describe 'kibera-pcs::install_pkgs' do
   let (:chef_run) { ChefSpec::ChefRunner.new.converge 'kibera-pcs::install_pkgs' }
-  it 'should do something' do
-    pending 'Your recipe examples go here.'
+  subject { chef_run }
+  ['gns3', 'libdvdcss2', 'mplayer-gui', 'non-free-codecs', 'w32codecs'].each do  |pkg|
+    it { expect(chef_run).to install_package pkg }
   end
 end
